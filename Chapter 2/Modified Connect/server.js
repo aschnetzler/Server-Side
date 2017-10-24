c893d37b6dae0db3cc512d244de7c6ef8b92be56
Alexa <aschnetzler2014@hotmail.com>
2017-10-24 13:03:12 -0500

    Chapter 2
    This is my chapter 2 code for my server side class

---
+++ Chapter 2/Modified Connect/server.js
@@ -0,0 +1,4 @@
+const connect = require('connect');
+const app = connect();
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
---
+++ Chapter 2/Modified Middleware/server.js
@@ -0,0 +1,14 @@
+const connect = require('connect');
+const app = connect();
+function logger(req, res, next) {
+console.log(req.method, req.url);
+next();
+};
+function helloWorld(req, res, next) {
+res.setHeader('Content-Type', 'text/plain');
+res.end('Hello World');
+};
+app.use(logger);
+app.use(helloWorld);
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
