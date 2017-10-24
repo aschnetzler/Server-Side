a0cf1257de4b6e7f9f6e8df0e2809a85b1a75fe2
Alexa <aschnetzler2014@hotmail.com>
2017-10-24 11:22:18 -0500

    Chapter 6
    This is the code I did for chapter 6

---
+++ Chapter 6/Passport/app/routes/users.server.routes.js
@@ -0,0 +1,98 @@
+const users = require('../../app/controllers/users.server.controller');
+
+passport = require('passport');
+
+module.exports = function (app) {
+
+
+
+    app.route('/signup')
+
+        .get(users.renderSignup)
+
+        .post(users.signup);
+
+    app.route('/signin')
+
+        .get(users.renderSignin)
+
+        .post(passport.authenticate('local', {
+
+            successRedirect: '/',
+
+            failureRedirect: '/signin',
+
+            failureFlash: true
+
+        }));
+
+    app.get('/signout', users.signout);
+
+
+
+    app.route('/users')
+
+        .post(users.create)
+
+        .get(users.list);
+
+
+
+    app.route('/users/:userId')
+
+        .get(users.read)
+
+        .put(users.update)
+
+        .delete(users.delete);
+
+    app.param('userId', users.userByID);
+
+
+
+    app.get('/oauth/facebook', passport.authenticate('facebook', {
+
+        scope:['emails'],
+
+        failureRedirect: '/signin'
+
+    }));
+
+    app.get('/oauth/facebook/callback', passport.authenticate('facebook',
+
+        {
+
+            failureRedirect: '/signin',
+
+            successRedirect: '/'
+
+        }));
+
+
+
+    app.get('/oauth/google', passport.authenticate('google', {
+
+        failureRedirect: '/signin',
+
+        scope: [
+
+            'https://www.googleapis.com/auth/userinfo.profile',
+
+            'https://www.googleapis.com/auth/userinfo.email'
+
+        ],
+
+    }));
+
+    app.get('/oauth/google/callback', passport.authenticate('google', {
+
+        failureRedirect: '/signin',
+
+        successRedirect: '/'
+
+    }));
+
+
+
+};
+
